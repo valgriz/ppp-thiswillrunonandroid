18be300f5120e1b498da07f1295483ce6a35859b
Steven Marturano <steven.marturano@gmail.com>
2015-01-10 19:34:45 -0500

    Fixed

--- src/com/twopercent/render/SoundPlayer.java
+++ src/com/twopercent/render/SoundPlayer.java
@@ -1,27 +1,29 @@
-
 package com.twopercent.render;
 
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-        
-public class SoundPlayer {
-    private static Media bounce = new Media(SoundPlayer.class.getResource("/res/sounds/BounceEffect.mp3").toString());
-    private static Media button = new Media(SoundPlayer.class.getResource("/res/sounds/ButtonSound.mp3").toString());
-    private static Media fallingPlatform = new Media(SoundPlayer.class.getResource("/res/sounds/FallingPlatform.mp3").toString());
-   // private static Media 
-    
-    public static void playBounce(){
-        new MediaPlayer(bounce).play();
-    }
 
-    public static void playButton(){
-        new MediaPlayer(button).play();
-    }
-    
-    public static void playFallingPlatform(){
-        MediaPlayer fallingPlat = new MediaPlayer(fallingPlatform);
-        fallingPlat.setVolume(0.1); fallingPlat.play();
-    }
-    
+public class SoundPlayer {
+	private static Media bounce = new Media(SoundPlayer.class.getResource("/res/sounds/BounceEffect.mp3").toString());
+	private static Media button = new Media(SoundPlayer.class.getResource("/res/sounds/ButtonSound.mp3").toString());
+	private static Media fallingPlatform = new Media(SoundPlayer.class.getResource("/res/sounds/FallingPlatform.mp3")
+			.toString());
+
+	// private static Media
+
+	public static void playBounce() {
+		new MediaPlayer(bounce).play();
+	}
+
+	public static void playButton() {
+		new MediaPlayer(button).play();
+	}
+
+	public static void playFallingPlatform() {
+		MediaPlayer fallingPlat = new MediaPlayer(fallingPlatform);
+		fallingPlat.setVolume(0.1);
+		fallingPlat.play();
+	}
+
 }
